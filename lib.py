ee40b7bd2b67b1440fdde75d5555420a11f90d73
Ina J <inajns@gmail.com>
2022-01-08 14:53:10 +0100

    Initial push

---
+++ lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    name = input("Who are you?")
+    if name != "":
+        print(f"Here's some motivation for you: You can do it {name}! 🙌")
+    else:
+        print ('Oops, I think you should try again')
+
+try_me()
